af7e0b22c8c07358ce25d1f7ef559e301e1b20d0
Natalia Palimaka <npalimaka@gmail.com>
2018-03-17 14:36:46 +0100

    db connector class

--- src/main/java/com/sda/jdbc/Main.java
+++ src/main/java/com/sda/jdbc/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Connection connection = null;
         Statement statement = null;
         try {
@@ -23,29 +23,56 @@ public class Main {
             String sql = "select * from customer";
             ResultSet resultSet = statement.executeQuery(sql);
 
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 System.out.print("Imię: " + resultSet.getString("first_name"));
                 System.out.println(" Nazwisko: " + resultSet.getString("last_name"));
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        try{
+        try {
             Class.forName(JDBC_DRIVER);
             connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
             statement = connection.createStatement();
             String sql = "select * from car order by mark asc";
             ResultSet resultSet = statement.executeQuery(sql);
 
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 System.out.println("Samochód: " + resultSet.getString("mark") + " "
                         + resultSet.getString("model"));
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
+        try {
+
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+         /*   String sql = "insert into customer (first_name, postal_code, address) values (?, ?, ?) ";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, "nowa wartość");
+            preparedStatement.setString(2, "00-000");
+            preparedStatement.setString(3, "nieznany");
+            preparedStatement.executeUpdate();*/
+            String sql0 = "update customer set last_name = 'ToJa' where first_name = 'Natalia'";
+            PreparedStatement preparedStatement0 = connection.prepareStatement(sql0);
+            preparedStatement0.executeUpdate();
+
+            String sql2 = "update car set model = 'Rdza' where model = 'Astra'";
+            PreparedStatement preparedStatement1 = connection.prepareStatement(sql2);
+            //  preparedStatement1.executeUpdate();
+            String sql3 = "delete from customer where first_name = 'nowa wartość'";
+            PreparedStatement preparedStatement2 = connection.prepareStatement(sql3);
+            //  preparedStatement2.executeUpdate();
+            String sql4 = "delete from car where mark = 'Toyota' and model = 'Yaris' and rate = 4";
+            PreparedStatement preparedStatement3 = connection.prepareStatement(sql4);
+            preparedStatement3.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
     }
